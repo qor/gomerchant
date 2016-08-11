4cc2b6c3c2c1f3041324b53b3e6b3b5753e6808b
Jinzhu <wosmvp@gmail.com>
2016-08-11 10:52:16 +0800

    Add logger as example to handle common things

--- gomerchant.go
+++ gomerchant.go
@@ -1,29 +1,53 @@
 package gomerchant
 
+import (
+	"log"
+	"os"
+)
+
 type Gomerchant struct {
 	PaymentGateway PaymentGateway
-	Config         Config
+	Config         *Config
 }
 
 type Config struct {
+	Logger Logger
+}
+
+type Logger interface {
+	Print(values ...interface{})
 }
 
 func New(paymentGateway PaymentGateway, config *Config) *Gomerchant {
+	if config.Logger == nil {
+		config.Logger = log.New(os.Stdout, "\r\n", 0)
+	}
+
 	return &Gomerchant{PaymentGateway: paymentGateway, Config: config}
 }
 
+// TBD: logger, common error validations...
+
 func (gomerchant *Gomerchant) Purchase(amount uint64, pm *PaymentMethod, params *PurchaseParams) (PurchaseResponse, error) {
-	return gomerchant.Purchase(amount, pm, params)
+	response, err := gomerchant.Purchase(amount, pm, params)
+	gomerchant.Config.Logger.Print("Purchase", pm, params, response, err)
+	return response, err
 }
 
 func (gomerchant *Gomerchant) Authorize(amount uint64, pm *PaymentMethod, params *AuthorizeParams) (AuthorizeResponse, error) {
-	return gomerchant.Authorize(amount, pm, params)
+	response, err := gomerchant.Authorize(amount, pm, params)
+	gomerchant.Config.Logger.Print("Authorize", pm, params, response, err)
+	return response, err
 }
 
 func (gomerchant *Gomerchant) Capture(transactionID string, params *CaptureParams) (CaptureResponse, error) {
-	return gomerchant.Capture(amount, pm, params)
+	response, err := gomerchant.Capture(transactionID, params)
+	gomerchant.Config.Logger.Print("Capture", transactionID, response, err)
+	return response, err
 }
 
 func (gomerchant *Gomerchant) Void(transactionID string, params *VoidParams) (VoidResponse, error) {
-	return gomerchant.Void(amount, pm, params)
+	response, err := gomerchant.Void(transactionID, params)
+	gomerchant.Config.Logger.Print("Void", transactionID, params, response, err)
+	return response, err
 }
