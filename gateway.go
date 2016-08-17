f1c55c995b4675a1cefcfc9c2e5ad426ccaa098b
bom_d_van <bom.d.van@gmail.com>
2016-08-17 10:57:08 +0900

    gomerchant: add docs

--- gateway.go
+++ gateway.go
@@ -1,6 +1,10 @@
 package gomerchant
 
 type PaymentGateway interface {
+	// The amount to charge as an integer (never a float). In the case of
+	// currencies that support fractional amounts, should be the integer
+	// amount of the smallest fractional (so, in the case of USD, the integer
+	// number of cents).
 	Purchase(amount uint64, pm *PaymentMethod, params *PurchaseParams) (PurchaseResponse, error)
 	Authorize(amount uint64, pm *PaymentMethod, params *AuthorizeParams) (AuthorizeResponse, error)
 	Capture(transactionID string, params *CaptureParams) (CaptureResponse, error)
