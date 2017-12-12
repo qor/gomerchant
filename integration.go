f4f745796c846f283493cd8f7295f0b6ed800110
Jinzhu <wosmvp@gmail.com>
2017-12-12 09:53:30 +0800

    Update integration API

--- integration.go
+++ integration.go
@@ -1,5 +1,21 @@
 package gomerchant
 
+import "net/http"
+
 // IntegrationGateway integration gateway
 type IntegrationGateway interface {
+	CheckoutURL(params CheckoutParams) (string, error)
+	VerifyNotification(req *http.Request) (bool error)
+	Refund(transactionID string, amount uint, params RefundParams) (RefundResponse, error)
+	Void(transactionID string, params VoidParams) (VoidResponse, error)
+	Query(transactionID string) (Transaction, error)
+}
+
+// CheckoutParams checkout params
+type CheckoutParams struct {
+	Amount      uint64
+	Currency    string
+	OrderID     string
+	Description string
+	Params
 }
